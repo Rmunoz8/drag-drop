075a1edbffda609044fe99c725247755f3c93c59
rmunoz8 <rmunoz@iesfuentesanluis.org>
2017-11-10 10:48:48 +0100

    -

--- app.js
+++ app.js
@@ -3,93 +3,96 @@ window.onload = function(){
 
     var dragged;
 
-  /* events fired on the draggable target */
   document.addEventListener("drag", function( event ) {
 
   }, false);
 
   document.addEventListener("dragstart", function( event ) {
 
-      // store a ref. on the dragged elem
       dragged = event.target;
-      // make it half transparent
-      event.target.style.opacity = .5;
+      dragged.style.opacity = 0.5;
+
   }, false);
 
   document.addEventListener("dragend", function( event ) {
-      // reset the transparency
+
       event.target.style.opacity = "";
 
   }, false);
 
-  /* events fired on the drop targets */
   document.addEventListener("dragover", function( event ) {
-      // prevent default to allow drop
+
       event.preventDefault();
 
   }, false);
 
   document.addEventListener("dragenter", function( event ) {
-//      console.log("Entra?");
-      // highlight potential drop target when the draggable element enters it
-      console.log(event.target.id);
-      if ( event.target.id == "contenedor" ) {
+
+      if(event.target.id == "hueco1" && dragged.id == "figura1"){
+            document.getElementById("corrector").style.backgroundColor = "green";
+      }
+       if(event.target.id == "hueco2" && dragged.id == "figura2"){
+        event.target.style.border = '3px dotted #555';
+      }
+      if(event.target.id == "hueco3" && dragged.id == "figura3"){
+        event.target.style.border = '3px dotted #555';
+      }
+      if(event.target.id == "hueco4" && dragged.id == "figura4"){
+        event.target.style.border = '3px dotted #555';
+      }
+  }, false);
+
+  document.addEventListener("dragleave", function( event ) {
+
+      if(event.target.id == "hueco1" || event.target.id == "hueco2" || event.target.id == "hueco3" || event.target.id == "hueco4"){
+          document.getElementById("corrector").style.backgroundColor = "#F7B577";
+      }else{
+          document.getElementById("corrector").style.backgroundColor = "red";
+      }
+
+
+  }, false);
+
+  document.addEventListener("drop", function( event ) {
+
+      event.preventDefault();
+
+      if ( event.target.id == "hueco1" && dragged.id == "figura1") {
+          dragged.parentNode.removeChild( dragged );
+          document.getElementById("con1").appendChild( dragged );
+          document.getElementById("hueco1").parentNode.removeChild(event.target);
+          document.getElementById("figura1").setAttribute("draggable", false);
+      }
+
+      if ( event.target.id == "hueco4" && dragged.id == "figura4") {
+            dragged.parentNode.removeChild( dragged );
+            document.getElementById("con4").appendChild( dragged );
+            document.getElementById("hueco4").parentNode.removeChild(event.target);
+            document.getElementById("figura4").setAttribute("draggable", false);
+
+
+      }
+
+      if ( event.target.id == "hueco3" && dragged.id == "figura3") {
+          dragged.parentNode.removeChild( dragged );
+          document.getElementById("con3").appendChild( dragged );
+          document.getElementById("hueco3").parentNode.removeChild(event.target);
+          document.getElementById("figura3").setAttribute("draggable", false);
+
+      }
+      if ( event.target.id == "hueco2" && dragged.id == "figura2") {
+          dragged.parentNode.removeChild( dragged );
+          document.getElementById("con2").appendChild( dragged );
+          document.getElementById("hueco2").parentNode.removeChild(event.target);
+          document.getElementById("figura2").setAttribute("draggable", false);
 
       }
 
 
       if ( event.target.id == "inicial" ) {
-
-      }
-
-  }, false);
-
-  document.addEventListener("dragleave", function( event ) {
-      // reset background of potential drop target when the draggable element leaves it
-      if ( event.target.id == "contenedor" ) {
-
-      }
-
-  }, false);
-
-  document.addEventListener("drop", function( event ) {
-      // prevent default action (open as link for some elements)
-      event.preventDefault();
-      // move dragged elem to the selected drop target
-      if ( event.target.id == "pezS" && dragged.id == "pez") {
-          dragged.parentNode.removeChild( dragged );
-          document.getElementById("contenedor").appendChild( dragged );
-          document.getElementById("pezS").parentNode.removeChild(event.target);
-
-      }
-
-      if ( event.target.id == "perroS" && dragged.id == "perro") {
-          dragged.parentNode.removeChild( dragged );
-          document.getElementById("contenedor").appendChild( dragged );
-          document.getElementById("perroS").parentNode.removeChild(event.target);
-
-      }
-
-      if ( event.target.id == "gatoS" && dragged.id == "gato") {
-          dragged.parentNode.removeChild( dragged );
-          document.getElementById("contenedor").appendChild( dragged );
-          document.getElementById("gatoS").parentNode.removeChild(event.target);
-
-      }
-      if ( event.target.id == "loroS" && dragged.id == "loro") {
-          dragged.parentNode.removeChild( dragged );
-          document.getElementById("contenedor").appendChild( dragged );
-          document.getElementById("loroS").parentNode.removeChild(event.target);
-
-      }
-
-//
-        if ( event.target.id == "inicial" ) {
           dragged.parentNode.removeChild( dragged );
           event.target.appendChild( dragged );
       }
-//
-
 
   }, false);
 
